c013864c2a50a3f03a6dc62607228b3d2cf688ae
Brian Meneses <brian.meneses@gmail.com>
2012-09-24 22:37:10 -0700

    refactored catalog.rb, extract method

--- lib/wine_api/catalog.rb
+++ lib/wine_api/catalog.rb
@@ -22,39 +22,14 @@ module WineApi
     #
     def get(params)
       
-      base_url = 'http://services.wine.com/api/beta2/service.svc/json/catalog'
-      params[:offset] == nil ? req_offset = 1 : req_offset = params[:offset]
-      params[:limit] == nil ? req_size = 100 : req_size = params[:limit]
-      req_sort = 'justIn|descending'
-
-      url = "#{base_url}?"
-
-
-
-      if (params[:product_id] == nil && params[:categories] != nil)
-        url += "filter=categories(#{params[:categories]})" + 
-                "&size=#{req_size}" + 
-                "&offset=#{req_offset}" +
-                "&sort=#{req_sort}" + 
-                "&apikey=#{@api_key}"
-      elsif (params[:product_id] != nil && params[:categories] == nil)
-        url += "filter=product(#{params[:product_id]})" +
-                "&apikey=#{@api_key}"
-      else
-        raise ArgumentError.new("Need to have a hash with one of" +
-                                   " :product_id or :categories")
-      end
-
-      puts url
+      request_url = create_url(params)
 
       begin
-        @json_response = HTTParty.get(URI.encode(url))
+        @json_response = HTTParty.get(URI.encode(request_url))
         rescue SocketError
           @response_hash = Hashie::Mash.new({})
         return
       end
-
-      
 
       @response_hash = Hashie::Mash.new(@json_response.to_hash)
       set_attrs(@response_hash)
@@ -70,6 +45,58 @@ module WineApi
         @offset = hash.Products.Offset
         @return_code = hash.Status.ReturnCode
       end
+
+      def create_url(params)
+        base_url = 'http://services.wine.com/api/beta2/service.svc/json/catalog?'
+        url = base_url
+
+        if  category_query?(params)
+          url = base_url + url_for_category_query(params)
+        elsif product_query?(params)
+          url = base_url + url_for_product_query(params)
+
+        else
+          raise ArgumentError.new("Need to have a hash with one of" +
+                                     " :product_id or :categories")
+        end
+        url
+      end
+
+      def category_query?(params)
+        params[:product_id] == nil && params[:categories] != nil
+      end
+
+      def product_query?(params)
+        params[:product_id] != nil && params[:categories] == nil
+      end
+
+      def url_for_category_query(params)
+        params[:offset] == nil ? req_offset = 1 : req_offset = params[:offset]
+        params[:limit]  == nil ? req_size = 100 : req_size = params[:limit]
+        req_sort = 'justIn|descending'
+        
+        url = "filter=categories(#{params[:categories]})" + 
+              "&size=#{req_size}" + 
+              "&offset=#{req_offset}" +
+              "&sort=#{req_sort}" + 
+              "&apikey=#{@api_key}"
+        url
+      end
+
+      def url_for_product_query(params)
+        url = "filter=product(#{params[:product_id]})" +
+              "&apikey=#{@api_key}"
+        url
+      end
+
+
+
+
+
+
+
+
+
 
   end
 
